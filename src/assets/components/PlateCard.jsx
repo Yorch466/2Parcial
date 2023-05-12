240c2fb6d6c6a3667a11dbf02da98e1ac494f221
yorch <jorgellave46@gmail.com>
2023-05-12 11:22:25 -0400

    primer componente

---
+++ src/assets/components/PlateCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const PlateCard = () => {
+  return (
+    <div className='plate-container'>
+        <div className="paint"></div>
+        <div className="sup-text">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+  )
+}
---
+++ src/assets/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css"
+
+export const Title = () => {
+  return (
+    <div className="header">
+      <p>Milk Shakes</p>
+      <hr />
+    </div>
+  )
+}
+
