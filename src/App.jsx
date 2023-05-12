c48ec3521dcddd0a130b5a8375f99273101ac31c
yorch <jorgellave46@gmail.com>
2023-05-12 11:25:39 -0400

    segundo componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Title } from "./assets/components/Title";
+import { PlateCard } from "./assets/components/PlateCard";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div className="container">
-        <Title />        
+        <Title />   
+        <PlateCard/>     
       </div>
     </>
   );
