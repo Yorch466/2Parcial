89337b2b3affc74c956a13e77088be4fefaab800
yorch <jorgellave46@gmail.com>
2023-05-12 11:36:41 -0400

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Title } from "./assets/components/Title";
-import { PlateCard } from "./assets/components/PlateCard";
+import { Title } from "./components/Title";
+import { PlateCard } from "./components/PlateCard";
 
 function App() {
   const [count, setCount] = useState(0);
